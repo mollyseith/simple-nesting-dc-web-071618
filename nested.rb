88a3ca02c2a5767e043598ec488deae17c8afa54
Molly Seither <molly.seither@centre.edu>
2018-07-15 21:39:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -54,7 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay][:languages][0]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
