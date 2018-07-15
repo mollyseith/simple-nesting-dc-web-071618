ce0ce3ef70c65f3c8d079765546b41160bd987e6
Molly Seither <molly.seither@centre.edu>
2018-07-15 21:43:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
-
-
 end
 
 def adding_to_dennis
@@ -126,5 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages[1]] = "Assembly"
+		 programmer_hash
 
 end
