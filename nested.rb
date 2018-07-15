1d039d904f98961a3f8539b8bed1accdb509039e
Molly Seither <molly.seither@centre.edu>
2018-07-15 21:36:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay, :known_for]
+		 programmer_hash[:alan_kay[0]]
 end
 
 def dennis_ritchies_language
